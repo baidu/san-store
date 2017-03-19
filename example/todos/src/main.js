a820de550110338d2bb63d94f5f4d7ab897b56fb
erik <erik168@163.com>
2017-03-20 01:28:57 +0800

    add example

---
+++ example/todos/src/main.js
@@ -0,0 +1,27 @@
+import san from 'san'
+
+// style
+require('font-awesome/css/font-awesome.min.css')
+require('./main.css')
+
+// actions
+import './components/todo/action'
+import './components/category/action'
+
+// route
+import List from './components/todo/List'
+import Form from './components/todo/Form'
+import AddCategory from './components/category/Add'
+import EditCategory from './components/category/Edit'
+
+import {router} from 'san-router'
+
+router.add({rule: '/', Component: List, target: '#app'});
+router.add({rule: '/todos/category/:category', Component: List, target: '#app'});
+router.add({rule: '/add', Component: Form, target: '#app'});
+router.add({rule: '/edit/:id', Component: Form, target: '#app'});
+router.add({rule: '/category/add', Component: AddCategory, target: '#app'});
+router.add({rule: '/category/edit', Component: EditCategory, target: '#app'});
+
+// start
+router.start();
