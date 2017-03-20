4a1ce27cdddaf9a858bf0d859cc334adea2200d9
erik <erik168@163.com>
2017-03-20 14:43:45 +0800

    connect granularity

--- test/connect.san.spec.js
+++ test/connect.san.spec.js
@@ -218,4 +218,159 @@ describe('Connect san component', () => {
             done();
         })
     });
+
+    store.addAction('for-connect-persons', persons => {
+        let builder = updateBuilder().set('persons', persons);
+        return builder;
+    });
+
+    it('components data should not infulence each other, and push item', done => {
+        store.addAction('for-connect-5', email => {
+            let builder = updateBuilder().push('persons[0].emails', email);
+            return builder;
+        });
+
+        let MyComponent = san.defineComponent({
+            template: '<dl><dt title="{{name}}">{{name}}</dt><dd san-for="email in emails" title="{{email}}">{{email}}</dd></dl>'
+        });
+
+        connect({
+            name: 'persons[0].name',
+            emails: 'persons[0].emails'
+        })(MyComponent);
+
+        store.dispatch('for-connect-persons', [
+            {name: 'erik', emails: ['erik168@163.com', 'errorrik@gmail.com']}
+        ]);
+
+        let wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+
+        let myComponent1 = new MyComponent();
+        myComponent1.attach(wrap);
+        let myComponent2 = new MyComponent();
+        myComponent2.attach(wrap);
+
+        let dts = wrap.getElementsByTagName('dt');
+        let dds = wrap.getElementsByTagName('dd');
+        expect(dts.length).toBe(2);
+        expect(dds.length).toBe(4);
+        expect(dts[0].title).toBe('erik');
+        expect(dts[1].title).toBe('erik');
+
+        expect(dds[0].title).toBe('erik168@163.com');
+        expect(dds[1].title).toBe('errorrik@gmail.com');
+        expect(dds[2].title).toBe('erik168@163.com');
+        expect(dds[3].title).toBe('errorrik@gmail.com');
+
+        store.dispatch('for-connect-5', 'xxx@xxx.com');
+
+        san.nextTick(() => {
+            let dts = wrap.getElementsByTagName('dt');
+            let dds = wrap.getElementsByTagName('dd');
+            expect(dts.length).toBe(2);
+            expect(dds.length).toBe(6);
+
+            expect(dds[0].title).toBe('erik168@163.com');
+            expect(dds[1].title).toBe('errorrik@gmail.com');
+            expect(dds[2].title).toBe('xxx@xxx.com');
+            expect(dds[3].title).toBe('erik168@163.com');
+            expect(dds[4].title).toBe('errorrik@gmail.com');
+            expect(dds[5].title).toBe('xxx@xxx.com');
+
+
+            myComponent2.data.push('emails', 'erik@erik.com');
+            san.nextTick(() => {
+                let dts = wrap.getElementsByTagName('dt');
+                let dds = wrap.getElementsByTagName('dd');
+                expect(dts.length).toBe(2);
+                expect(dds.length).toBe(7);
+
+                expect(dds[0].title).toBe('erik168@163.com');
+                expect(dds[1].title).toBe('errorrik@gmail.com');
+                expect(dds[2].title).toBe('xxx@xxx.com');
+                expect(dds[3].title).toBe('erik168@163.com');
+                expect(dds[4].title).toBe('errorrik@gmail.com');
+                expect(dds[5].title).toBe('xxx@xxx.com');
+                expect(dds[6].title).toBe('erik@erik.com');
+
+                myComponent1.dispose();
+                myComponent2.dispose();
+                document.body.removeChild(wrap);
+                done();
+            });
+
+        })
+    });
+
+    it('components data should not infulence each other, remove item', done => {
+        store.addAction('for-connect-6', email => {
+            let builder = updateBuilder().remove('persons[0].emails', email);
+            return builder;
+        });
+
+        let MyComponent = san.defineComponent({
+            template: '<dl><dt title="{{person.name}}">{{person.name}}</dt><dd san-for="email in person.emails" title="{{email}}">{{email}}</dd></dl>'
+        });
+
+        connect({
+            person: 'persons[0]'
+        })(MyComponent);
+
+        store.dispatch('for-connect-persons', [
+            {name: 'erik', emails: ['erik168@163.com', 'errorrik@gmail.com']}
+        ]);
+
+        let wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+
+        let myComponent1 = new MyComponent();
+        myComponent1.attach(wrap);
+        let myComponent2 = new MyComponent();
+        myComponent2.attach(wrap);
+
+        let dts = wrap.getElementsByTagName('dt');
+        let dds = wrap.getElementsByTagName('dd');
+        expect(dts.length).toBe(2);
+        expect(dds.length).toBe(4);
+        expect(dts[0].title).toBe('erik');
+        expect(dts[1].title).toBe('erik');
+
+        expect(dds[0].title).toBe('erik168@163.com');
+        expect(dds[1].title).toBe('errorrik@gmail.com');
+        expect(dds[2].title).toBe('erik168@163.com');
+        expect(dds[3].title).toBe('errorrik@gmail.com');
+
+        store.dispatch('for-connect-6', 'erik168@163.com');
+
+        san.nextTick(() => {
+            let dts = wrap.getElementsByTagName('dt');
+            let dds = wrap.getElementsByTagName('dd');
+            expect(dts.length).toBe(2);
+            expect(dds.length).toBe(2);
+
+            expect(dds[0].title).toBe('errorrik@gmail.com');
+            expect(dds[1].title).toBe('errorrik@gmail.com');
+
+
+            myComponent2.data.push('person.emails', 'erik@erik.com');
+            san.nextTick(() => {
+                let dts = wrap.getElementsByTagName('dt');
+                let dds = wrap.getElementsByTagName('dd');
+                expect(dts.length).toBe(2);
+                expect(dds.length).toBe(3);
+
+
+                expect(dds[0].title).toBe('errorrik@gmail.com');
+                expect(dds[1].title).toBe('errorrik@gmail.com');
+                expect(dds[2].title).toBe('erik@erik.com');
+
+                myComponent1.dispose();
+                myComponent2.dispose();
+                document.body.removeChild(wrap);
+                done();
+            });
+
+        })
+    });
 });
