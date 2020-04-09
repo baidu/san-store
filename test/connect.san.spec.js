f356e7e03f6f1faa74388fa76c25ca160952d105
errorrik <errorrik@gmail.com>
2020-04-09 13:07:58 +0800

    add test case 4 babel trans code

--- test/connect.san.spec.js
+++ test/connect.san.spec.js
@@ -2,6 +2,146 @@ import {store, connect} from 'san-store';
 import san from 'san';
 import {updateBuilder} from 'san-update';
 
+function _typeof(obj) {
+    "@babel/helpers - typeof";
+    if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
+        _typeof = function _typeof(obj) {
+            return typeof obj;
+        };
+    } else {
+        _typeof = function _typeof(obj) {
+            return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol": typeof obj;
+        };
+    }
+    return _typeof(obj);
+}
+
+function _get(target, property, receiver) {
+    if (typeof Reflect !== "undefined" && Reflect.get) {
+        _get = Reflect.get;
+    } else {
+        _get = function _get(target, property, receiver) {
+            var base = _superPropBase(target, property);
+            if (!base) return;
+            var desc = Object.getOwnPropertyDescriptor(base, property);
+            if (desc.get) {
+                return desc.get.call(receiver);
+            }
+            return desc.value;
+        };
+    }
+    return _get(target, property, receiver || target);
+}
+
+function _superPropBase(object, property) {
+    while (!Object.prototype.hasOwnProperty.call(object, property)) {
+        object = _getPrototypeOf(object);
+        if (object === null) break;
+    }
+    return object;
+}
+
+function _createSuper(Derived) {
+    return function() {
+        var Super = _getPrototypeOf(Derived),
+        result;
+        if (_isNativeReflectConstruct()) {
+            var NewTarget = _getPrototypeOf(this).constructor;
+            result = Reflect.construct(Super, arguments, NewTarget);
+        } else {
+            result = Super.apply(this, arguments);
+        }
+        return _possibleConstructorReturn(this, result);
+    };
+}
+
+function _possibleConstructorReturn(self, call) {
+    if (call && (_typeof(call) === "object" || typeof call === "function")) {
+        return call;
+    }
+    return _assertThisInitialized(self);
+}
+
+function _assertThisInitialized(self) {
+    if (self === void 0) {
+        throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    }
+    return self;
+}
+
+function _isNativeReflectConstruct() {
+    if (typeof Reflect === "undefined" || !Reflect.construct) return false;
+    if (Reflect.construct.sham) return false;
+    if (typeof Proxy === "function") return true;
+    try {
+        Date.prototype.toString.call(Reflect.construct(Date, [],
+        function() {}));
+        return true;
+    } catch(e) {
+        return false;
+    }
+}
+
+function _getPrototypeOf(o) {
+    _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf: function _getPrototypeOf(o) {
+        return o.__proto__ || Object.getPrototypeOf(o);
+    };
+    return _getPrototypeOf(o);
+}
+
+function _inherits(subClass, superClass) {
+    if (typeof superClass !== "function" && superClass !== null) {
+        throw new TypeError("Super expression must either be null or a function");
+    }
+    subClass.prototype = Object.create(superClass && superClass.prototype, {
+        constructor: {
+            value: subClass,
+            writable: true,
+            configurable: true
+        }
+    });
+    if (superClass) _setPrototypeOf(subClass, superClass);
+}
+
+function _setPrototypeOf(o, p) {
+    _setPrototypeOf = Object.setPrototypeOf ||
+    function _setPrototypeOf(o, p) {
+        o.__proto__ = p;
+        return o;
+    };
+    return _setPrototypeOf(o, p);
+}
+
+function _instanceof(left, right) {
+    if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) {
+        return !! right[Symbol.hasInstance](left);
+    } else {
+        return left instanceof right;
+    }
+}
+
+function _classCallCheck(instance, Constructor) {
+    if (!_instanceof(instance, Constructor)) {
+        throw new TypeError("Cannot call a class as a function");
+    }
+}
+
+function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
+    }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+}
+
 describe('Connect san component', () => {
     store.addAction('reset-for-connect', () => {
         let resetBuilder = updateBuilder()
@@ -43,6 +183,99 @@ describe('Connect san component', () => {
         class RawComponent extends san.Component {
 
         }
+        RawComponent.template =  '<u title="{{name}}-{{email}}">{{name}}-{{email}}</u>';
+
+        let MyComponent = connect.san({
+            name: 'name',
+            email: 'emails[0]'
+        })(
+            RawComponent
+        );
+
+
+        let myComponent = new MyComponent();
+        let wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        let u = wrap.getElementsByTagName('u')[0];
+        expect(myComponent.data.get('name')).toBe('errorrik');
+        expect(u.title).toBe('errorrik-errorrik@gmail.com');
+
+        myComponent.dispose();
+        document.body.removeChild(wrap);
+    });
+
+
+    it('data should be ready when component init, component declare as class and trans by babel', () => {
+        
+        var RawComponent = /*#__PURE__*/function (_san$Component) {
+            _inherits(RawComponent, _san$Component);
+
+            var _super = _createSuper(RawComponent);
+
+            function RawComponent() {
+                _classCallCheck(this, RawComponent);
+
+                return _super.apply(this, arguments);
+            }
+
+            return RawComponent;
+        }(san.Component);
+        
+        RawComponent.template =  '<u title="{{name}}-{{email}}">{{name}}-{{email}}</u>';
+
+        let MyComponent = connect.san({
+            name: 'name',
+            email: 'emails[0]'
+        })(
+            RawComponent
+        );
+
+
+        let myComponent = new MyComponent();
+        let wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        let u = wrap.getElementsByTagName('u')[0];
+        expect(myComponent.data.get('name')).toBe('errorrik');
+        expect(u.title).toBe('errorrik-errorrik@gmail.com');
+
+        myComponent.dispose();
+        document.body.removeChild(wrap);
+    });
+
+    it('data should be ready when component init, component declare as class and trans by babel, extends more than once', () => {
+        
+        var BaseComponent = /*#__PURE__*/function (_san$Component) {
+            _inherits(BaseComponent, _san$Component);
+          
+            var _super = _createSuper(BaseComponent);
+          
+            function BaseComponent() {
+              _classCallCheck(this, BaseComponent);
+          
+              return _super.apply(this, arguments);
+            }
+          
+            return BaseComponent;
+          }(san.Component);
+          
+          var RawComponent = /*#__PURE__*/function (_BaseComponent) {
+            _inherits(RawComponent, _BaseComponent);
+          
+            var _super2 = _createSuper(RawComponent);
+          
+            function RawComponent() {
+              _classCallCheck(this, RawComponent);
+          
+              return _super2.apply(this, arguments);
+            }
+          
+            return RawComponent;
+          }(BaseComponent);
+        
         RawComponent.template =  '<u title="{{name}}-{{email}}">{{name}}-{{email}}</u>';
 
         let MyComponent = connect.san({
